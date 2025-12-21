6f49f93c6e676423c67f3f0956adc6af1be4d935
yagmur-b <yagmurbasoral1@gmail.com>
2025-12-21 19:02:50 +0300

    Add comment to STUDENT_BILLING table creation
    Added comment to clarify the purpose of the table.

--- 02_Relationships/student_billing.sql
+++ 02_Relationships/student_billing.sql
@@ -1,5 +1,7 @@
+-- Links students to their specific billing transactions
 CREATE TABLE STUDENT_BILLING (
     TransactionID NUMBER,
     StudentID NUMBER NOT NULL,
-    BillingType VARCHAR2(20) NOT NULL -- e.g., 'Room Rent', 'Dining'
+    BillingType VARCHAR2(20) NOT NULL
 );
+
