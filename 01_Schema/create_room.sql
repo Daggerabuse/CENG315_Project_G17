614a84243d7b64bc14d92a6f5eb50943af453a6d
yagmur-b <yagmurbasoral1@gmail.com>
2025-12-21 13:47:58 +0300

    Update create_room.sql

--- 01_Schema/create_room.sql
+++ 01_Schema/create_room.sql
@@ -4,11 +4,12 @@
 CREATE TABLE ROOM (
     RoomID NUMBER,
     Block VARCHAR2(5) NOT NULL, 
-    Floor NUMBER(2) NOT NULL, -- floor ismi sorun olur mu acaba
+    FloorNumber NUMBER(2) NOT NULL,
     MaxCapacity NUMBER(2) NOT NULL,
     CurrentCapacity NUMBER(2) NOT NULL,
     OccupancyStatus VARCHAR(20) NOT NULL,
     RoomType VARCHAR2(20) NOT NULL,
     Price NUMBER(10, 2) NOT NULL
 );
+
 
